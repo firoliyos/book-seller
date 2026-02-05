a00bc74360dd7c1a9ce9b52196fa31d51cadba29
firoliyos <firatesfaye44@gmail.com>
2026-02-05 12:37:16 +0300

    Best-sellers

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
+import OurBestSellers from '../components/OurBestSellers'
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banner/>
+      <OurBestSellers/>
     </>
   )
 }
