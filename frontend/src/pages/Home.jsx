5fab31c6ed3b8177f37bf4b7a84ec6a52ef73142
firoliyos <firatesfaye44@gmail.com>
2026-02-06 10:28:34 +0300

    HomeBooks imported in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import OurBestSellers from '../components/OurBestSellers'
+import HomeBooks from '../components/HomeBooks'
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banner/>
       <OurBestSellers/>
+      <HomeBooks/>
     </>
   )
 }
